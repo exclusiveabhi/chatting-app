0db351f5792e55a0c9aa90dc8ac17b6b86dd221f
exclusiveabhi <imexclusiveabhi@gmail.com>
2024-08-10 12:16:52 +0530

    10/08/2024

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Button } from './components/ui/button'
+import { Outlet } from 'react-router-dom'
 
 function App() {
   return (
-    
-    <Button>Click me</Button>
+    <>
+    <Outlet></Outlet>
+    </>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,38 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Signin from './auth/sign-in/Signin.jsx' //file name changed hai!
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Signin from "./auth/sign-in/Signin.jsx"; //file name changed hai!
+import Home from "./home/Home.jsx"
+import Dashboard from "./dashboard/Dashboard.jsx";
 
 //router yaha hai!
 
 const router = createBrowserRouter([
   {
-path:"/",
-element: <App> </App>
+    
+    element: <App> </App>,
+    children:[
+      {
+        path: "/", //iska mtlb hai agar / route pr jaoge to <Home/> component load hona chaiyee!
+        element: <Home></Home>
+      },
+      {
+        path: "/dashboard",
+        element: <Dashboard></Dashboard>
+      }
+
+    ]
   },
   {
     path: "/auth/sign-in",
-    element: <Signin></Signin>
-  }
-])
+    element: <Signin></Signin>,
+  },
+]);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router}></RouterProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
