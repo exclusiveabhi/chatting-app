ad21fbfd74ada941a2c5585520570c53b61b85d1
exclusiveabhi <imexclusiveabhi@gmail.com>
2024-08-10 13:10:33 +0530

    10/08/2024

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import React from "react";
 import { Button } from "./components/ui/button";
 import { Navigate, Outlet } from "react-router-dom";
 import { useUser } from "@clerk/clerk-react";
+import Header from "./components/custom/Header";
 
 function App() {
   const { user, isLoaded, isSignedIn } = useUser();
 
-  if (!isSignedIn) {
+  if (!isSignedIn && isLoaded) {
     return <Navigate to={"/auth/sign-in"}></Navigate>
   }
 
   return (
     <>
+    <Header></Header>
       <Outlet></Outlet>
     </>
   );
