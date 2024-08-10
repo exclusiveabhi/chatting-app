0bc3b95aaf593fada790e6871ed65e3ad1acba84
exclusiveabhi <imexclusiveabhi@gmail.com>
2024-08-10 12:06:09 +0530

    10/08/2024

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,24 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Signin from './auth/sign-in/Signin.jsx' //file name changed hai!
+
+//router yaha hai!
+
+const router = createBrowserRouter([
+  {
+path:"/",
+element: <App> </App>
+  },
+  {
+    path: "/auth/sign-in",
+    element: <Signin></Signin>
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </StrictMode>,
 )
