d2ff32386054dc218c6c940ed4aefe15d06130c2
exclusiveabhi <imexclusiveabhi@gmail.com>
2024-08-16 12:51:46 +0530

    16/08/2024

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -2,7 +2,7 @@
 
 import { Router } from 'express';
 const router = Router();
-import User, { findOne } from '../models/User';
+import User, { findOne } from '../models/UserModel';
 import { sign } from 'jsonwebtoken';
 
 // Register User
